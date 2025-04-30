9627ed003ed5529ef60b17d7970365e39e7ae6ea
TheFollan <a.a-t@bk.ru>
2025-04-30 19:42:48 +0300

    completed implementation

--- include/float.h
+++ include/float.h
@@ -7,18 +7,22 @@
 
 typedef struct {
     uint32_t sign : 1;
-    uint32_t exponent : 8;
-    uint32_t mantissa : 23;
-}floatComponents;
+    uint32_t exp : 8;
+    uint32_t mant : 23;
+}float_components;
 
-uint32_t floatToBits (float);
-float bitsToFloat (uint32_t); 
-void decomposeFloat (float, floatComponents*);
-float composeFloat (const floatComponents*);
-float sumFloat (float, float); 
-bool isPositivZero (floatComponents*);
-bool isNegativeZero (floatComponents*);
-bool isPositiveInf (float, floatComponents*);
-bool isNegativeInf (float, floatComponents*);
+float sum_float (float, float); 
 
-
+static uint32_t float_to_bits (float);
+static float bits_to_float (uint32_t); 
+static void decompose_float (float, float_components*);
+static float compose_float (const float_components*);
+static bool is_positive_zero (float_components*);
+static bool is_negative_zero (float_components*);
+static bool is_positive_inf (float, float_components*);
+static bool is_negative_inf (float, float_components*);
+static bool is_subnormal (float_components*);
+static int32_t get_real_exponent (float_components*);
+static uint64_t get_mantissa(float_components*);
+static void alignment_exp (int32_t*, uint64_t*, int32_t*, uint64_t*);
+static void normalization_result(uint64_t*, int32_t*);
